50cbd8738f661b15a89e754d9ca8630a01634370
Gavin-Yeah <gavinjiadaye@gmail.com>
2024-08-25 16:23:09 -0400

    PORTAL-76-Angular-Add-MUI-to-HR-pages Add MUI to pages

--- src/app/component/employee-profile-detail/employee-profile-detail.component.ts
+++ src/app/component/employee-profile-detail/employee-profile-detail.component.ts
@@ -6,17 +6,56 @@ import { ProfileService } from 'src/app/services/profile.service';
 @Component({
   selector: 'app-employee-profile-detail',
   templateUrl: './employee-profile-detail.component.html',
-  styleUrls: ['./employee-profile-detail.component.css']
+  styleUrls: ['./employee-profile-detail.component.css'],
 })
 export class EmployeeProfileDetailComponent implements OnInit {
+  employeeDetail!: EmployeeDetail;
 
-  employeeDetail!: EmployeeDetail
+  // employeeDetail: EmployeeDetail = {
+  //   userId: { email: 'fsdfsdcsd' },
+  //   firstName: 'csdccscs',
+  //   lastName: 'cscsdcs',
+  //   middleName: 'sdcc',
+  //   preferredName: 'sdccs',
+  //   address: {
+  //     street: '456 Elm St2c',
+  //     city: 'csOthertownd',
+  //     state: 'csNYd',
+  //     zip: '67890dcsc',
+  //   },
+  //   cellPhoneNumber: '555-234-5678dcs',
+  //   workPhoneNumber: '555-876-5432dcsc',
+  //   car: { make: 'Honda', model: 'Civic', color: 'Red' },
+  //   ssn: '987-65-432',
+  //   dateOfBirth: '706334400000',
+  //   gender: 'Male',
+  //   citizenship: {
+  //     visaStatus: 'F1',
+  //     startDate: '1723694400000',
+  //     endDate: '1723003200000',
+  //   },
+  //   driverLicense: {
+  //     hasDriverLicense: true,
+  //     licenseNumber: 'S9876543',
+  //     expirationDate: '1861833600000',
+  //   },
+  //   emergencyContacts: [
+  //     {
+  //       firstName: 'Robertdcsc',
+  //       lastName: 'dSmithcsc',
+  //       middleName: '',
+  //       phone: 'csdcsdcsc',
+  //       email: 'csdc@csac.comdcs',
+  //       relationship: 'Fatherdcsc',
+  //     },
+  //   ],
+  // };
 
   constructor(
     private route: ActivatedRoute,
     private profileService: ProfileService,
-    private router: Router,
-  ) { }
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
@@ -24,6 +63,45 @@ export class EmployeeProfileDetailComponent implements OnInit {
       this.profileService.getEmployeeById(id).subscribe(
         (data: EmployeeDetail) => {
           this.employeeDetail = data;
+          // {
+          //   userId: { email: 'fsdfsdcsd' },
+          //   firstName: 'csdccscs',
+          //   lastName: 'cscsdcs',
+          //   middleName: 'sdcc',
+          //   preferredName: 'sdccs',
+          //   address: {
+          //     street: '456 Elm St2c',
+          //     city: 'csOthertownd',
+          //     state: 'csNYd',
+          //     zip: '67890dcsc',
+          //   },
+          //   cellPhoneNumber: '555-234-5678dcs',
+          //   workPhoneNumber: '555-876-5432dcsc',
+          //   car: { make: 'Honda', model: 'Civic', color: 'Red' },
+          //   ssn: '987-65-432',
+          //   dateOfBirth: '706334400000',
+          //   gender: 'Male',
+          //   citizenship: {
+          //     visaStatus: 'F1',
+          //     startDate: '1723694400000',
+          //     endDate: '1723003200000',
+          //   },
+          //   driverLicense: {
+          //     hasDriverLicense: true,
+          //     licenseNumber: 'S9876543',
+          //     expirationDate: '1861833600000',
+          //   },
+          //   emergencyContacts: [
+          //     {
+          //       firstName: 'Robertdcsc',
+          //       lastName: 'dSmithcsc',
+          //       middleName: '',
+          //       phone: 'csdcsdcsc',
+          //       email: 'csdc@csac.comdcs',
+          //       relationship: 'Fatherdcsc',
+          //     },
+          //   ],
+          // };
         },
         (error) => {
           console.error('Error fetching employee details:', error);
@@ -43,5 +121,4 @@ export class EmployeeProfileDetailComponent implements OnInit {
   goBack(): void {
     this.router.navigate(['/employee-profiles']);
   }
-
 }
--- src/app/component/employee-profiles/employee-profiles.component.ts
+++ src/app/component/employee-profiles/employee-profiles.component.ts
@@ -5,39 +5,152 @@ import { ProfileService } from 'src/app/services/profile.service';
 @Component({
   selector: 'app-employee-profiles',
   templateUrl: './employee-profiles.component.html',
-  styleUrls: ['./employee-profiles.component.css']
+  styleUrls: ['./employee-profiles.component.css'],
 })
 export class EmployeeProfilesComponent implements OnInit {
-
-  employees: EmployeeSummary[] = []
+  employees: EmployeeSummary[] = [];
   filteredEmployees: EmployeeSummary[] = [];
+  // employees: EmployeeSummary[] = [
+
+  //   {
+  //     userid: 'fsdfsdcsd',
+  //     firstName: 'csdccscs',
+  //     lastName: 'cscsdcs',
+  //     middleName: 'sdcc',
+  //     preferredName: 'sdccs',
+  //     visaStatus: 'pending',
+  //     email: 'csd@cac.com',
+
+  //     cellPhoneNumber: '555-234-5678dcs',
+
+  //     ssn: '987-65-432',
+  //   },
+  //   {
+  //     userid: 'fsdfsdcsd',
+  //     firstName: 'csdccscs',
+  //     lastName: 'cscsdcs',
+  //     middleName: 'sdcc',
+  //     preferredName: 'sdccs',
+  //     visaStatus: 'pending',
+  //     email: 'csd@cac.com',
+
+  //     cellPhoneNumber: '555-234-5678dcs',
+
+  //     ssn: '987-65-432',
+  //   },
+  // ];
+  // filteredEmployees: EmployeeSummary[] = [
+  //   {
+  //     userid: 'fsdfsdcsd',
+  //     firstName: 'csdccscs',
+  //     lastName: 'cscsdcs',
+  //     middleName: 'sdcc',
+  //     preferredName: 'sdccs',
+  //     visaStatus: 'pending',
+  //     email: 'csd@cac.com',
+
+  //     cellPhoneNumber: '555-234-5678dcs',
+
+  //     ssn: '987-65-432',
+  //   },
+  //   {
+  //     userid: 'fsdfsdcsd',
+  //     firstName: 'csdccscs',
+  //     lastName: 'cscsdcs',
+  //     middleName: 'sdcc',
+  //     preferredName: 'sdccs',
+  //     visaStatus: 'pending',
+  //     email: 'csd@cac.com',
+
+  //     cellPhoneNumber: '555-234-5678dcs',
+
+  //     ssn: '987-65-432',
+  //   },
+  // ];
   searchTerm: string = '';
 
-  constructor(private profileService: ProfileService) { }
+  constructor(private profileService: ProfileService) {}
 
   ngOnInit(): void {
-    this.fetchAllEmployees()
+    this.fetchAllEmployees();
   }
 
   fetchAllEmployees(): void {
     this.profileService.getAllEmployees().subscribe(
       (employees: EmployeeSummary[]) => {
-        this.employees = employees; 
-        this.filteredEmployees = employees; 
+        this.employees = employees;
+        this.filteredEmployees = employees;
+        // [
+        //   {
+        //     userid: 'fsdfsdcsd',
+        //     firstName: 'csdccscs',
+        //     lastName: 'cscsdcs',
+        //     middleName: 'sdcc',
+        //     preferredName: 'sdccs',
+        //     visaStatus: 'pending',
+        //     email: 'csd@cac.com',
+
+        //     cellPhoneNumber: '555-234-5678dcs',
+
+        //     ssn: '987-65-432',
+        //   },
+        //   {
+        //     userid: 'fsdfsdcsd',
+        //     firstName: 'csdccscs',
+        //     lastName: 'cscsdcs',
+        //     middleName: 'sdcc',
+        //     preferredName: 'sdccs',
+        //     visaStatus: 'pending',
+        //     email: 'csd@cac.com',
+
+        //     cellPhoneNumber: '555-234-5678dcs',
+
+        //     ssn: '987-65-432',
+        //   },
+        // ];
+        // this.filteredEmployees = [
+        //   {
+        //     userid: 'fsdfsdcsd',
+        //     firstName: 'csdccscs',
+        //     lastName: 'cscsdcs',
+        //     middleName: 'sdcc',
+        //     preferredName: 'sdccs',
+        //     visaStatus: 'pending',
+        //     email: 'csd@cac.com',
+
+        //     cellPhoneNumber: '555-234-5678dcs',
+
+        //     ssn: '987-65-432',
+        //   },
+        //   {
+        //     userid: 'fsdfsdcsd',
+        //     firstName: 'csdccscs',
+        //     lastName: 'cscsdcs',
+        //     middleName: 'sdcc',
+        //     preferredName: 'sdccs',
+        //     visaStatus: 'pending',
+        //     email: 'csd@cac.com',
+
+        //     cellPhoneNumber: '555-234-5678dcs',
+
+        //     ssn: '987-65-432',
+        //   },
+        // ];
       },
       (error) => {
-        console.error('Failed to fetch employees', error); 
+        console.error('Failed to fetch employees', error);
       }
     );
   }
 
   filterEmployees(): void {
     const term = this.searchTerm.toLowerCase();
-    this.filteredEmployees = this.employees.filter(employee => 
-      employee.firstName.toLowerCase().includes(term) ||
-      employee.lastName.toLowerCase().includes(term) ||
-      (employee.preferredName && employee.preferredName.toLowerCase().includes(term))
+    this.filteredEmployees = this.employees.filter(
+      (employee) =>
+        employee.firstName.toLowerCase().includes(term) ||
+        employee.lastName.toLowerCase().includes(term) ||
+        (employee.preferredName &&
+          employee.preferredName.toLowerCase().includes(term))
     );
   }
-
 }
