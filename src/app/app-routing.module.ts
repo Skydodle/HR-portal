efa7c94f296ae5d180e55f7df5df4d56abf2c53f
Gavin-Yeah <gavinjiadaye@gmail.com>
2024-08-25 23:18:46 -0400

    PORTAL-76 update styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,22 +9,24 @@ import { HousingManagementComponent } from './component/housing-management/housi
 import { HousingDetailComponent } from './component/housing-detail/housing-detail.component';
 import { OnboardingApplicationComponent } from './component/onboarding-application/onboarding-application.component';
 
-
 const routes: Routes = [
-  {path: '', redirectTo:'/login', pathMatch:'full'}, // Default route redirects to login
-  {path: 'login', component: LoginComponent},
-  {path: 'employee-profiles', component: EmployeeProfilesComponent},
-  {path: 'employee-profiles/:id', component: EmployeeProfileDetailComponent},
-  {path: 'visa-status-management', component: VisaStatusManagementComponent},
-  {path: 'hiring-management', component: HiringManagementComponent},
-  {path: 'housing-management', component: HousingManagementComponent},
-  {path: 'housing-detail/:id', component: HousingDetailComponent},
-  {path: 'onboarding-applications', component: OnboardingApplicationComponent},
-  {path: '**', redirectTo:'/login'}// Wildcard route redirects to login (for any unmatched routes)
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route redirects to login
+  { path: 'login', component: LoginComponent },
+  { path: 'employee-profiles', component: EmployeeProfilesComponent },
+  { path: 'employee-profiles/:id', component: EmployeeProfileDetailComponent },
+  { path: 'visa-status-management', component: VisaStatusManagementComponent },
+  { path: 'hiring-management', component: HiringManagementComponent },
+  { path: 'housing-management', component: HousingManagementComponent },
+  { path: 'housing-detail/:id', component: HousingDetailComponent },
+  {
+    path: 'onboarding-applications',
+    component: OnboardingApplicationComponent,
+  },
+  { path: '**', redirectTo: '/login' }, // Wildcard route redirects to login (for any unmatched routes)
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
